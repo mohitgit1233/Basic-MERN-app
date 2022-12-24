0f3047dd93409c3357a9fbc33f4a76681c098697
Mohit Wadhwa <mohitwadhwa1233@gmail.com>
2022-12-23 19:30:17 -0800

    forms

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,78 @@
 import React from 'react'
 
+import { useState, useEffect } from 'react'
+
+
 function Login() {
+
+  const [formData, setFormData] = useState({
+
+    email: '',
+    password: '',
+
+  })
+
+  const {  email, password } = formData
+
+
+
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <section className='heading'>
+        <h1>
+          Login
+        </h1>
+        <p>Please fill in the details</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+         
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              placeholder='Enter your email'
+              onChange={onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              placeholder='Enter password'
+              onChange={onChange}
+            />
+          </div>
+       
+          <div className='form-group'>
+            <button type='submit' className='btn btn-block'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
